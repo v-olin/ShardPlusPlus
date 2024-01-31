a87fffa3785c85303141848af7d5e82112adb2d8
fatonhoti <hotifaton@outlook.com>
2024-01-31 12:46:59 +0100

    Fix bug in premake and remove GLFW as it's not a dependency anymore

--- premake5.lua
+++ premake5.lua
@@ -17,10 +17,6 @@ project "Shard"
 	kind "SharedItems"
 	language "C++"
 
-	-- This project will not be built
-	-- targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	-- objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
 	files
 	{
 		"%{prj.name}/**.h",
@@ -31,52 +27,15 @@ project "Shard"
 	
 	includedirs
 	{
-		-- I have swapped prj.name to wks.location because
-		-- VS is dumb as shit and can't read
-		"%{wks.location}/Shard/vendor/SDL2-2.28.5/include",
-		"%{wks.location}/Shard/vendor/SDL2_ttf-2.22.0/include",
-		"%{wks.location}/Shard/vendor/SDL2_image-2.8.2/include",
-		"%{wks.location}/Shard/vendor/glm",
-		"%{wks.location}/Shard/vendor/stb_image/include",
-		"%{wks.location}/Shard/vendor/tiny_obj/include",
-		"%{wks.location}/Shard/vendor/glew/include",
-		"%{wks.location}/Shard/include"
+		"%{prj.name}/vendor/SDL2-2.28.5/include",
+		"%{prj.name}/vendor/SDL2_ttf-2.22.0/include",
+		"%{prj.name}/vendor/SDL2_image-2.8.2/include",
+		"%{prj.name}/vendor/glm",
+		"%{prj.name}/vendor/stb_image/include",
+		"%{prj.name}/vendor/tiny_obj/include",
+		"%{prj.name}/vendor/glew/include",
+		"%{prj.name}/include"
 	}
-	
-	-- No need for binaries, will not be built
-	-- libdirs
-	-- {
-	-- 	"%{prj.name}/vendor/SDL2-2.28.5/lib/x64",
-	-- 	"%{prj.name}/vendor/SDL2_ttf-2.22.0/lib/x64",
-	-- 	"%{prj.name}/vendor/SDL2_image-2.8.2/lib/x64",
-	-- 	-- "%{prj.name}/vendor/GLFW/lib-vc2022",
-	-- 	"%{prj.name}/vendor/glew/lib"
-	-- }
-	
-	-- links
-	-- {
-	-- 	"SDL2.lib",
-	-- 	"SDL2_ttf.lib",
-	-- 	"SDL2_image.lib",
-	-- 	"opengl32.lib",
-	-- 	"glew32.lib",
-	-- 	-- "glfw3.lib",
-	-- 	"glu32.lib"
-	-- }
-	
-	-- Will not be built
-	-- filter "system:windows"
-	-- 	cppdialect "C++20"
-	-- 	staticruntime "On"
-	-- 	systemversion "latest"
-	
-	-- filter "configurations:Debug"
-	-- 	symbols "On"
-	-- 	optimize "Off"
-	
-	-- filter "configurations:Release"
-	-- 	symbols "Off"
-	-- 	optimize "On"
 
 project "Game"
 	location "Game"
@@ -91,7 +50,6 @@ project "Game"
 	{
 		"%{prj.name}/**.h",
 		"%{prj.name}/**.cpp",
-		-- Not sure if these below are necessary (from glm/inc)
 		"%{prj.name}/**.hpp", 
 		"%{prj.name}/**.inl"
 	}
@@ -105,31 +63,10 @@ project "Game"
 		"%{wks.location}/Shard/vendor/stb_image/include",
 		"%{wks.location}/Shard/vendor/tiny_obj/include",
 		"%{wks.location}/Shard/vendor/glew/include",
-		-- glew > glfw?
-		-- "%{prj.name}/vendor/GLFW/include/GLFW",
-		-- include all files from Shared Items Project
 		"%{wks.location}/Shard/include",
 		"%{prj.name}/include"
 	}
-	
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
-		
-		postbuildcommands
-		{
-			-- No Shard.dll to copy
-			-- ("{COPY} ../bin/" .. outputdir .. "/Shard/Shard.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}")
-			-- Inherited from old Shard dll project
-            ("{COPY} ../bin/" .. outputdir .. "/Shard/SDL2.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
-            ("{COPY} ../bin/" .. outputdir .. "/Shard/SDL2_ttf.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
-            ("{COPY} ../bin/" .. outputdir .. "/Shard/SDL2_image.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
-            ("{COPY} ../bin/" .. outputdir .. "/Shard/glew32.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}")
-		}
 
-	-- Now Game has to depend on binaries
-	-- since they are not compiled into Shard.dll
     libdirs
 	{
 		"%{wks.location}/Shard/vendor/SDL2-2.28.5/lib/x64",
@@ -157,6 +94,19 @@ project "Game"
 		"glew32.lib",
 		"glu32.lib"
     }
+	
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+		
+		postbuildcommands
+		{
+			("{COPY} ../Shard/vendor/SDL2-2.28.5/lib/x64/SDL2.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
+			("{COPY} ../Shard/vendor/SDL2_ttf-2.22.0/lib/x64/SDL2_ttf.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
+			("{COPY} ../Shard/vendor/SDL2_image-2.8.2/lib/x64/SDL2_image.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
+			("{COPY} ../Shard/vendor/glew/lib/glew32.dll" .. " ../bin/" .. outputdir .. "/%{prj.name}"),
+		}
 	
 	filter "configurations:Debug"
 		defines { "DEBUG" }
