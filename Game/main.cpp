3b28b3dfc6379886b7c21982caadfa9984cb5b78
eliskleen <eliskleen00@gmail.com>
2024-02-12 15:01:07 +0100

    SAP 'should' work now :)

--- Game/main.cpp
+++ Game/main.cpp
@@ -117,8 +117,12 @@ public:
 		Shard::Logger::log("COLLSION");
 	}
     
-	void onCollisionExit(std::shared_ptr<Shard::PhysicsBody> body) override {};
-    void onCollisionStay(std::shared_ptr<Shard::PhysicsBody> body) override {};
+	void onCollisionExit(std::shared_ptr<Shard::PhysicsBody> body) override {
+		Shard::Logger::log("Exit colision");
+	};
+    void onCollisionStay(std::shared_ptr<Shard::PhysicsBody> body) override {
+		Shard::Logger::log("stay colision");
+	};
 	void checkDestroyMe() override {};
 	void initialize()  override {};
 	void update()  override {};
@@ -142,8 +146,6 @@ int Test1() {
 
 	auto gob1 = CreateGOB({ 5.0f, 10.0f, 10.0f });
 	auto gob2 = CreateGOB({ 50.0f, 10.0f, 10.0f });
-	//manager.addPhysicsObject(gob1->body_->shared_from_this());
-	//manager.addPhysicsObject(gob2->body_->shared_from_this());
 
 	// Tick
 	manager.update();
@@ -152,6 +154,8 @@ int Test1() {
 	if (manager.collision_last_frame == 0)
 		passed = 1;
 
+	manager.removePhysicsObject(gob1->body_);
+	manager.removePhysicsObject(gob2->body_);
 	// Result?
 	std::cout << "Result test 1: " << std::to_string(passed == 1) << "\n";
 	return passed;
@@ -164,29 +168,57 @@ int Test2() {
 
 	auto gob1 = CreateGOB({ 45.0f, 5.0f, 5.0f });
 	auto gob2 = CreateGOB({ 50.0f, 10.0f, 10.0f });
-	//manager.addPhysicsObject(gob1->body_->shared_from_this());
-	//manager.addPhysicsObject(gob2->body_->shared_from_this());
 
 	// Tick
 	manager.update();
 
 	// Check collisions
-	if (manager.collision_last_frame > 0)
+	if (manager.collision_last_frame == 1 )
 		passed = 1;
 
+	manager.removePhysicsObject(gob1->body_);
+	manager.removePhysicsObject(gob2->body_);
 	// Result?
 	std::cout << "Result test 2: " << std::to_string(passed == 1) << "\n";
 	return passed;
+}
+
+int Test3() {
+	int passed{ 0 };
+
+	auto gob1 = CreateGOB({ 41.0f, 48.0f, 48.0f });
+	//gob1->body_->pass_through = true;
+	auto gob2 = CreateGOB({ 50.0f, 50.0f, 50.0f });
+
+	// Tick
+	manager.update();
+	auto col_after_first = manager.collision_last_frame;
+	manager.update();
+	auto col_after_sec = manager.collision_last_frame;
+	manager.update();
+
+	// Check collisions
+	if (col_after_sec == 0 && col_after_first == 1)
+		passed = 1;
+
+	manager.removePhysicsObject(gob1->body_);
+	manager.removePhysicsObject(gob2->body_);
+	// Result?
+	std::cout << "Result test 3: " << std::to_string(passed == 1) << "\n";
+	return passed;
+
 }
 
 int main() {
 
 	std::cout << "Testing shit... 0 means it failed and 1 means it passed.\n\n";
 	
-	int t1 = Test1(); // Should fail, no collision.
-	int t2 = Test2(); // Should pass, expecting collision.
+	//int t1 = Test1(); // Should fail, no collision.
+	//int t2 = Test2(); // Should pass, expecting collision.
+	int t3 = Test3(); // Should pass, expecting collision.
 	
 	std::cout << "\nhaha, you suck! your tests failed lol (safe assumption being made here)\n";
+	std::cin;
 
 	// Everything will always be fine.
 	return 0;
